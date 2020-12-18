1d262ef9a6b73e4697ac5b490c08e3030badfac3
latumak <56345611+latumak@users.noreply.github.com>
2020-12-18 02:22:55 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function openNav () {
+	var x = document.getElementById ("myLinks");
+
+	if (x.style.display === "block") {
+		x.style.display ="none";
+	} else {
+		x.style.display = "block";
+	}
+
+}
